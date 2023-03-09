a51025f9c61156d4bbe7fba454e0d40d30b369ea
Mohsin Shah <m.shah@blinkpayment.co.uk>
2023-03-09 15:39:32 +0000

    Moved envs to blink

---
+++ config/blink.php
@@ -0,0 +1,9 @@
+<?php
+
+return [ 
+    'server' => env('BLINK_SERVER'),
+    'api_key' => env('BLINK_API_KEY'),
+    'secret_key' => env('BLINK_SECRET_KEY'),
+    'return_url' => env('BLINK_RETURN_URL'),
+    'notification_url' => env('BLINK_NOTIFICATION_URL'),
+];
