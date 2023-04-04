01258429ea8c15fdd7deb9925c1f0d797926c099
Mohsin Shah <m.shah@blinkpayment.co.uk>
2023-04-04 15:08:08 +0100

    Changed res to transaction_id

--- app/Http/Controllers/ReturnController.php
+++ app/Http/Controllers/ReturnController.php
@@ -9,7 +9,7 @@ class ReturnController extends Controller
 {
     public function store(Request $request)
     {
-        print_r($request->res . '<br />');
+        print_r($request->transaction_id . '<br />');
         print_r($request->status . '<br />');
         print_r($request->note . '<br />');
         print_r(urldecode($request->merchant_data) . '<br />');
